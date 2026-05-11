6f7be66c92e0e8b9d90203337d438e0d28e9834d
Lucas Santos <79930094+luc-santos@users.noreply.github.com>
2026-05-11 20:52:22 -0300

    Add files via upload

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,12 @@
+ft_putchar(int c);
+
+void	ft_print_alphabet(void)
+{
+	int	c;
+
+	c = 97;
+	while (c <= 122)
+	{
+		ft_putchar(c);
+	}
+}
---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,12 @@
+ft_putchar(int c);
+
+void	ft_print_numbers(void)
+{
+	int	c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		ft_putchar(c);
+	}
+}
